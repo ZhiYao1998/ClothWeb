91cc49c7ff96554a2ac57bd7f0fd4bdbf0b7326e
eddie@5xxruby.tw <sappaser0919515706@gmail.com>
2021-10-20 23:45:40 +0800

    This is a beginning, upload all the files of the project so far

---
+++ cont2.php
@@ -0,0 +1,27 @@
+<?php
+$checkbox_select=$_POST["Color"];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=clothweb", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully"."<br>";
+
+  $sql = "INSERT INTO color (color1,color2,color3,color4,color5,color6,color7,color8,color9,color10,color11,color12) 
+  VALUES ('$checkbox_select[0]','$checkbox_select[1]','$checkbox_select[2]','$checkbox_select[3]','$checkbox_select[4]','$checkbox_select[5]','$checkbox_select[6]','$checkbox_select[7]','$checkbox_select[8]','$checkbox_select[9]','$checkbox_select[10]','$checkbox_select[11]')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  //echo "New record created successfully";
+
+  $last_id = $conn->lastInsertId();
+  echo "New record created successfully. Last inserted ID is: " . $last_id;
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+$conn = null;
+?>
